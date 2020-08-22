11b52097f7ccd406d6bacd7d112e1ec8cf53325b
Aloysius Chan <alloystory@gmail.com>
2020-08-22 23:03:16 +0800

    edited assignment 0

--- Assignment 0/Checksum.py
+++ Assignment 0/Checksum.py
@@ -2,10 +2,6 @@ import sys
 from zlib import crc32
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 Checksum.py <filepath>")
-        exit(1)
-
     with open(sys.argv[1], "rb") as f:
         checksum = crc32(f.read())
         print(checksum)
--- Assignment 0/PacketExtr.py
+++ Assignment 0/PacketExtr.py
@@ -1,16 +1,20 @@
 import sys
 
 if __name__ == "__main__":
-    if sys.stdin.buffer.read1(6).decode() != "Size: ":
-        raise Exception("Wrong header")
-    num_bytes = 0
     while True:
-        data = sys.stdin.buffer.read1(1).decode()
-        if data == "B":
+        header = sys.stdin.buffer.read1(6)
+        if len(header) == 0:
             break
-        num_bytes = num_bytes * 10 + int(data)
-    
-    while num_bytes > 0:
-        packet = sys.stdin.buffer.read1(min(num_bytes, 1 * 1024 * 1024))
-        print(packet)
-        num_bytes -= len(packet)
+        
+        num_bytes = 0
+        while True:
+            data = sys.stdin.buffer.read1(1).decode()
+            if data == "B":
+                break
+            num_bytes = num_bytes * 10 + int(data)
+        
+        while num_bytes > 0:
+            packet = sys.stdin.buffer.read1(min(num_bytes, 1 * 1024 * 1024))
+            sys.stdout.buffer.write(packet)
+            sys.stdout.buffer.flush()
+            num_bytes -= len(packet)
